70be31a5ffdb475b791bec5ba9ce017fdda983ec
EstebanDarioRuizParedesCAMPUS <edrp.campus15@gmail.com>
2023-07-21 13:05:38 -0500

    MVC clase

---
+++ Facturartemis/frontend/API.js
@@ -0,0 +1,33 @@
+const url = "http://localhost:5000/api/categorias/";
+
+
+export const obtainCategories = async () => {
+ 
+};
+
+
+export const nuevaCategoria = async (categoria) => {
+  
+};
+
+
+export const deleteCategory = async (id) => {
+  
+};
+
+
+
+export const obtenerCategory = async (id) => {
+ 
+};
+
+
+
+export const editarCategory = async (category) => {
+ 
+};
+
+
+
+
+
